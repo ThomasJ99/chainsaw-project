df82ac2d609306c87f4a7f3ed85b6e027ce11929
ThomasJ99 <xmaslink1999@gmail.com>
2026-01-29 08:45:04 +0100

    movedhero banner into a ui file to clean up my homepage code and for experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import data from "@/data/volumes.json";
 import CardGrid from "@/components/ui/card-grid";
+import Hero from "@/components/ui/hero";
 
 export default function Home() {
   // const volumes = data.volumes;
@@ -11,55 +12,10 @@ export default function Home() {
 
   return (
     <main>
-      {/* Hero */}
-      <header className="py-16 px-4 bg-linear-to-b from-orange-600  via-yellow-500 to-lime-300">
-        <section className="container mx-auto px-4 grid items-center lg:grid-cols-2 gap-8 font-oswald drop-shadow-2xl">
-          <div>
-            <span className="opacity-95 text-2xl italic font-bold">
-              Immerse yourself in the world of
-            </span>
-            <h1 className="text-6xl xl:text-7xl font-bold leading-tight text-balance tracking-wide grid col-auto uppercase">
-              Chainsaw Man{" "}
-              <span className="font-noto-sans-jp">チェンソーマン</span>
-            </h1>
-            <p className="opacity-90 text-2xl">
-              By Tatsuki Fujimoto{" "}
-              <span className="font-noto-sans-jp">藤本 タツキ</span>
-            </p>
-          </div>
+      <Hero/>
 
-          <Image
-            className="border-3 border-black rounded-2xl shadow-2xl"
-            src="https://boolintunes.com/wp-content/uploads/2023/01/Chainsaw-Man-Banner-1-1.png"
-            alt="test"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            width={1500}
-            height={843}
-            preload={true}
-            loading="eager"
-          />
-        </section>
-      </header>
-
-      {/* This line of code replaces the other code that commented out below, it imports a component */}
+      {/* Creates a grid of cards, it imports a component using a function */}
       <CardGrid volumes={volumes} />
-      {/* <section className="container mx-auto">
-        <h2>Manga Volumes</h2>
-        <ul className="grid grid-cols-[repeat(auto-fill,minmax(35ch,1fr))]">
-          {volumes.map((volume) => (
-            <li key={volume.id}>
-              <Image
-                className=""
-                src={volume.image}
-                width={180}
-                height={284}
-                alt={`${volume.volume} of the manga series Chainsaw Man, written by Tatsuki Fujimoto`}
-              />
-              <h3>{volume.volume}</h3>
-            </li>
-          ))}
-        </ul>
-      </section> */}
     </main>
   );
 }
---
+++ components/ui/hero.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <header className="py-16 px-4 bg-linear-to-b from-orange-600  via-yellow-500 to-lime-300">
+      <section className="container mx-auto px-4 grid items-center lg:grid-cols-2 gap-8 font-oswald drop-shadow-2xl">
+        <div>
+          <span className="text-white/95 text-2xl italic font-bold">
+            Immerse yourself in the world of
+          </span>
+          <h1 className="text-6xl xl:text-7xl font-bold leading-tight text-balance tracking-wide grid col-auto uppercase">
+            Chainsaw Man{" "}
+            <span className="font-noto-sans-jp">チェンソーマン</span>
+          </h1>
+          <p className="text-white/90 text-2xl">
+            By Tatsuki Fujimoto{" "}
+            <span className="font-noto-sans-jp">藤本 タツキ</span>
+          </p>
+        </div>
+
+        <Image
+          className="border-3 border-black rounded-2xl shadow-2xl"
+          src="https://boolintunes.com/wp-content/uploads/2023/01/Chainsaw-Man-Banner-1-1.png"
+          alt="test"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          width={1500}
+          height={843}
+          preload={true}
+          loading="eager"
+        />
+      </section>
+    </header>
+  );
+}
