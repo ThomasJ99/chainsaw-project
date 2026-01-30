4c68d9304ea97fc7d83337e9015239955d5241f3
ThomasJ99 <xmaslink1999@gmail.com>
2026-01-30 14:50:09 +0100

    started work on the /volume/ pages, currently only writing out some text and cover image

--- app/volume/[id]/page.tsx
+++ app/volume/[id]/page.tsx
@@ -1,5 +1,6 @@
 import data from "@/data/volumes.json";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 // From https://nextjs.org/docs/app/api-reference/file-conventions/page#reading-searchparams-and-params-in-client-components
 
@@ -14,4 +15,21 @@ export default async function VolumePage({
 
   // If theres no volume, call the notFound function and return
   if (!volume) notFound();
+  //else return below..
+  return (
+    <main className="container mx-auto py-20">
+      <article className="grid md:grid-cols-3 ">
+        <div className="">
+          <Image className="w-full" src={volume.image} height={189} width={258} alt=""></Image>
+          <h1 className="text-5xl my-5 text-center">{volume.volume}</h1>
+          <p>{volume.description}</p>
+        </div>
+
+        <div className="text-center">
+          <button className="">Buy</button>
+          <button>Put in cart</button>
+        </div>
+      </article>
+    </main>
+  );
 }
