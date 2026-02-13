431f45683ce29f24bf61b49b20f5ce08ca3aee3a
ThomasJ99 <xmaslink1999@gmail.com>
2026-02-13 15:03:43 +0100

    added most of the hooks my price slider should need, state, params, pathname and router, i also check min/maxurl to be either my params or a default value

--- components/ui/price-slider.tsx
+++ components/ui/price-slider.tsx
@@ -1,4 +1,7 @@
-"use client"
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 // What is my sliders role before I start implementing it?
 // My getProducts holds the min/max price, so its job should be to render correctly in the URL by making sure the URL is correct
@@ -6,5 +9,23 @@
 // Perhaps ill have two sliders which each determine min/max with a input field which shows the current value for each slider
 
 export default function PriceSlider() {
-    
+  // States with a current value and a function which updates it
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
+
+  // Reads the searchParams - URL
+  const searchParams = useSearchParams();
+
+  // I need the current path to rebuild the URL correctly
+  const pathName = usePathname();
+
+  // Allows us to change the server value, meaning we can manipulate the URL
+  const router = useRouter();
+
+  const minURL = Number(searchParams.get("price_min") || 0);
+  const maxURL = Number(searchParams.get("price_max") || 100000);
+
+  const priceChange = () => {
+    // TODO: Implement logic here...
+  };
 }
